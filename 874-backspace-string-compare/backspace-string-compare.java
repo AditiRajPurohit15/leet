607d80766ff17d0674c6efde579ee2a5bba4907e
Aditi Raj Purohit <aditipurohit1505@gmail.com>
2026-07-30 22:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (79.20%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,21 +1,44 @@
 class Solution {
-    public String createStr(String s){
-    StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch!='#'){
-                sb.append(ch);
-            }else{
-                if(sb.length()==0)continue;
-                else sb.deleteCharAt(sb.length()-1);
-            }
-        }
-        return sb.toString();
-    }
     public boolean backspaceCompare(String s, String t) {
-        String s1=createStr(s);
-        String t1=createStr(t);
+        int i=s.length()-1;
+        int j=t.length()-1;
 
-        return s1.equals(t1);
+        int skipS=0;
+        int skipT=0;
+
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    skipS++;
+                    i--;
+                }else if(skipS>0){
+                    skipS--;
+                    i--;
+                }else{
+                    break;
+                }
+            }
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    skipT++;
+                    j--;
+                }else if(skipT>0){
+                    skipT--;
+                    j--;
+                }else{
+                    break;
+                }
+            }
+
+            if(i>=0 && j>=0){
+                if(s.charAt(i)!=t.charAt(j))return false;
+            }
+            else if(i>=0 ||j>=0){
+                return false;
+            }
+            i--;
+            j--;
+        }
+        return true;
     }
 }
