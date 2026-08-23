c391e201424da2713db2756d13fc69c8658f661e
Aditi Raj Purohit <aditipurohit1505@gmail.com>
2026-08-23 14:43:46 +0530

    Time: 76 ms (11.73%) | Memory: 88.5 MB (92.21%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,19 +1,20 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int i=0,j=0,cnt=0;
-        Map<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int ans=0,i=0,j=0;
         while(j<nums.length){
             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
 
-            while(i<j && map.get(nums[j])>k){
+            while(map.get(nums[j])>k){
                 map.put(nums[i],map.get(nums[i])-1);
-                if(map.get(nums[i])==0)map.remove(nums[i]);
+                if(map.get(nums[i])==0){
+                    map.remove(nums[i]);
+                }
                 i++;
             }
-
-            cnt=Math.max(cnt,j-i+1);
+            ans=Math.max(ans,j-i+1);
             j++;
         }
-        return cnt;
+        return ans;
     }
 }
