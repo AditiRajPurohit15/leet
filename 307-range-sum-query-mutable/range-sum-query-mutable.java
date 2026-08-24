488d4a0b16ea36b602298015e81857343ec28f54
Aditi Raj Purohit <aditipurohit1505@gmail.com>
2026-08-24 07:13:35 +0530

    Time: 94 ms (49.01%) | Memory: 133.8 MB (69.53%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,14 +1,25 @@
 class NumArray {
-
     int n;
     int[] segTree;
-
+    public NumArray(int[] nums) {
+        n=nums.length;
+        segTree=new int[4*n];
+        buildTree(0,0,n-1,segTree,nums);
+    }
+    
+    public void update(int index, int val) {
+        update(index,val,0,0,n-1);
+    }
+    
+    public int sumRange(int left, int right) {
+        return query(left,right,0,0,n-1);
+    }
     public void buildTree(int i,int l,int r,int[] segTree,int[] nums){
         if(l==r){
             segTree[i]=nums[l];
             return;
         }
-        int mid=l+(r-l)/2;
+        int mid=l + (r-l)/2;
         buildTree(2*i+1,l,mid,segTree,nums);
         buildTree(2*i+2,mid+1,r,segTree,nums);
 
@@ -22,30 +33,15 @@ class NumArray {
         int mid=l+(r-l)/2;
         if(idx<=mid){
             update(idx,val,2*i+1,l,mid);
-        }else{
+        }else
             update(idx,val,2*i+2,mid+1,r);
-        }
-        segTree[i]=segTree[2*i+1]+segTree[2*i+2];
+            segTree[i]=segTree[2*i+1]+segTree[2*i+2];
     }
     public int query(int start,int end,int i,int l,int r){
         if(l>end || r<start)return 0;
         if(l>=start && r<=end)return segTree[i];
-        int  mid=l+(r-l)/2;
-        return query(start,end,2*i+1,l,mid) + query(start,end,2*i+2,mid+1,r);
-    }
-
-    public NumArray(int[] nums) {
-        n=nums.length;
-        segTree=new int[4*n];
-        buildTree(0,0,n-1,segTree,nums);
-    }
-    
-    public void update(int index, int val) {
-        update(index,val,0,0,n-1);
-    }
-    
-    public int sumRange(int left, int right) {
-        return query(left,right,0,0,n-1);
+        int mid=l+(r-l)/2;
+        return query(start,end,2*i+1,l,mid)+query(start,end,2*i+2,mid+1,r);
     }
 }
 
