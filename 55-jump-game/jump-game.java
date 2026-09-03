792005a9d91b25bf15195e0fb00d37c8e1c15bd5
Aditi Raj Purohit <aditipurohit1505@gmail.com>
2026-09-03 14:41:58 +0530

    Time: 2 ms (89.02%) | Memory: 47.5 MB (95.61%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(i>maxReach)return false;
             maxReach=Math.max(maxReach,i+nums[i]);
-            if(maxReach>=nums.length-1)return true;
+            if(maxReach>nums.length)return true;
         }
         return true;
     }
