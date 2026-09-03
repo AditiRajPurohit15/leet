8fada825ae724d1e98f39da3f6efff11160bed23
Aditi Raj Purohit <aditipurohit1505@gmail.com>
2026-09-03 09:21:34 +0530

    Time: 3 ms (92.82%) | Memory: 46.7 MB (59.11%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,31 +1,25 @@
 class Solution {
-    public void allCombination(List<List<Integer>> ans,int[] nums,int i){
-        if(i==nums.length){
-            List<Integer> list=new ArrayList<>();
-            for(int n:nums)list.add(n);
-            ans.add(list);
+    List<List<Integer>> ans=new ArrayList<>();
+    public void solve(int[] nums,boolean[] used,List<Integer> list){
+        if(list.size()==nums.length){
+            ans.add(new ArrayList<>(list));
             return;
         }
 
-        Set<Integer> set=new HashSet<>();
-        for(int j=i;j<nums.length;j++){
-            if(set.contains(nums[j]))continue;
-
-            set.add(nums[j]);
-            swap(i,j,nums);
-            allCombination(ans,nums,i+1);
-            swap(i,j,nums);
+        for(int i=0;i<nums.length;i++){
+            if(used[i])continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1])continue;
+            list.add(nums[i]);
+            used[i]=true;
+            solve(nums,used,list);
+            used[i]=false;
+            list.remove(list.size()-1);
         }
-        // set.add(nums[i]);
-    }
-    public void swap(int i,int j,int[] nums){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        allCombination(ans,nums,0);
+        Arrays.sort(nums);
+        boolean[] used=new boolean[nums.length];
+        solve(nums,used,new ArrayList<>());
         return ans;
     }
 }
